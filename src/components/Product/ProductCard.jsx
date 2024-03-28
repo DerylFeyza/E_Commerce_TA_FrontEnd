1257d83491e57d9bd4259855cfa653ab34956208
DerylFeyza <muhammadderylfeyza@gmail.com>
2024-03-28 10:46:52 +0700

    card design

--- src/components/Product/ProductCard.jsx
+++ src/components/Product/ProductCard.jsx
@@ -1,21 +1,35 @@
 import PropTypes from "prop-types";
 import { BASE_API } from "../../utils/http-common";
 import { Link } from "react-router-dom";
+import { GrLocation } from "react-icons/gr";
+import "./Product.css";
 
 const ProductCard = ({ product }) => {
 	const IMAGEURL = `${BASE_API}/produk/image/${product.gambar_barang}`;
 	return (
-		<div className="card">
-			<img src={IMAGEURL} className="card-img-top" alt={product.nama_barang} />
-			<div className="card-body">
-				<h5 className="card-title">{product.nama_barang}</h5>
-				<h5 className="card-title">{product.id}</h5>
-				<p className="card-text">Category: {product.kategori}</p>
-				<p className="card-text">Price: ${product.harga}</p>
-				<Link to={`/products/${product.id}`} className="btn btn-primary">
-					View Details
-				</Link>
-			</div>
+		<div className="product-card-container">
+			<Link
+				to={`/products/${product.id}`}
+				className="product-card-link"
+				style={{ textDecoration: "none" }}
+			>
+				<div className="card shadow bg-body-tertiary product-card border-0">
+					<img
+						src={IMAGEURL}
+						className="card-img-top "
+						alt={product.nama_barang}
+						style={{ objectFit: "cover", width: "100%", height: "400px" }}
+					/>
+					<div className="card-body product-card-details">
+						<h5 className="card-title">{product.nama_barang}</h5>
+						<p className="card-text price">Rp. {product.harga}</p>
+						<p className="card-text ">Category: {product.kategori}</p>
+						<p>
+							<GrLocation /> Jakarta Pusat
+						</p>
+					</div>
+				</div>
+			</Link>
 		</div>
 	);
 };
