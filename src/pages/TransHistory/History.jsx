e4dfd4e2b7a6dac6fe3098784c1dc485c53964f5
DerylFeyza <muhammadderylfeyza@gmail.com>
2024-04-27 17:48:17 +0700

    History design

--- src/pages/TransHistory/History.jsx
+++ src/pages/TransHistory/History.jsx
@@ -1,6 +1,7 @@
 import { useState, useEffect } from "react";
 import { getTransactionHistory } from "../../services/receipt";
 import HistoryCard from "./HistoryCard";
+import purchaseSuccessImage from "../../assets/purchase-success.png";
 
 const Home = () => {
 	const [transactionHistory, setTransactionHistory] = useState([]);
@@ -24,13 +25,38 @@ const Home = () => {
 
 	return (
 		<>
-			{console.log(transactionHistory)}
-
-			<div className="transaction-container">
-				<div>
-					{transactionHistory.reverse().map((transaction, index) => (
-						<HistoryCard key={index} historyData={transaction} />
-					))}
+			<div className="history-container mx-auto mt-5 pt-5">
+				<div className="flex flex-wrap mx-3 mb-5">
+					<div className="w-full max-w-full px-3 mb-6 mx-auto">
+						<div className="relative flex-[1_auto] flex flex-col break-words min-w-0 bg-clip-border rounded-[.95rem] bg-white m-5">
+							<div className="relative flex flex-col min-w-0 break-words border border-dashed bg-clip-border rounded-2xl border-stone-200 bg-light/30">
+								<div className="px-9 pt-5 flex justify-between items-stretch flex-wrap min-h-[70px] pb-0 bg-transparent">
+									<h3 className="flex flex-col items-start justify-center m-2 ml-0 font-medium text-xl/tight text-dark">
+										<span className="mr-3 font-semibold text-4xl text-black">
+											Transaction History
+										</span>
+									</h3>
+								</div>
+								<div className="flex-auto block py-8 pt-6 px-9">
+									<table className="w-full my-0 align-middle text-dark border-neutral-200">
+										<thead className="align-bottom">
+											<tr className="font-semibold text-secondary-dark">
+												<th className="pb-3 pr-10 text-end text-black"></th>
+												<th className="pb-3 min-w-[100px] text-black">Total</th>
+												<th className="pb-3 text-black">Date</th>
+												<th className="pb-3 text-black">Action</th>
+											</tr>
+										</thead>
+										<tbody className="align-middle overflow-y-auto">
+											{transactionHistory.map((transaction, index) => (
+												<HistoryCard key={index} historyData={transaction} />
+											))}
+										</tbody>
+									</table>
+								</div>
+							</div>
+						</div>
+					</div>
 				</div>
 			</div>
 		</>
--- src/pages/TransHistory/HistoryCard.jsx
+++ src/pages/TransHistory/HistoryCard.jsx
@@ -3,44 +3,49 @@ import purchaseSuccessImage from "../../assets/purchase-success.png";
 import { Link } from "react-router-dom";
 
 const HistoryCard = ({ historyData }) => {
-	const dateString = new Date(historyData.updatedAt).toLocaleDateString(
+	const dateString = new Date(historyData.createdAt).toLocaleDateString(
 		"en-US",
 		{ timeZone: "Asia/Jakarta" }
 	);
-	const timeString = new Date(historyData.updatedAt).toLocaleTimeString(
+	const timeString = new Date(historyData.createdAt).toLocaleTimeString(
 		"en-US",
 		{ timeZone: "Asia/Jakarta" }
 	);
 
 	return (
-		<div className="purchase-success-container">
-			<div className="purchase-success">
-				<div className="image-container">
-					<img
-						src={purchaseSuccessImage}
-						alt="Purchase success"
-						className="success-image"
-					/>
-				</div>
-				<h3>Purchase success</h3>
-				<p>
-					{dateString} on {timeString} UTC+7
-				</p>
-				<p>$ {historyData.totalharga}</p>
-				<div className="button-container-history">
-					<Link to={`/receipt/${historyData.id}`} className="btn btn-success">
-						View Details
-					</Link>
-				</div>
-			</div>
-		</div>
+		<tr className="border-b border-neutral-200">
+			<td className="py-4 pr-10 text-black flex items-center justify-center flex-col">
+				<img
+					src={purchaseSuccessImage}
+					className="w-[155px] h-[155px] inline-block shrink-0 rounded-2xl"
+					alt="purchase-success-image"
+				/>
+				<p className="font-bold mt-2 text-center">Purchase Successful</p>
+			</td>
+
+			<td className="py-4 text-black pr-3">Rp. {historyData.totalharga}</td>
+			<td className="py-4 text-black pr-3">
+				{dateString}
+				<br />
+				{timeString}
+			</td>
+			<td className="py-4 text-black">
+				<Link
+					to={`/receipt/${historyData.id}`}
+					className="bg-blue hover:bg-darkGrayishBlue text-white font-bold py-2 px-4 rounded"
+					style={{ textDecoration: "none" }}
+				>
+					View Details
+				</Link>
+			</td>
+		</tr>
 	);
 };
 
 HistoryCard.propTypes = {
 	historyData: PropTypes.shape({
 		id: PropTypes.number.isRequired,
-		updatedAt: PropTypes.string.isRequired,
+		createdAt: PropTypes.string.isRequired,
 		totalharga: PropTypes.number.isRequired,
 	}).isRequired,
 };
